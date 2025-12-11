c74f2526fa9fb58d66122f26501d45bdf64eff70
tanzimsiamm <siam88536@gmail.com>
2025-12-11 12:34:39 -0800

    basic project setup done

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+
+//health check
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+
+
+export default app;
